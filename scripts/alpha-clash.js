b24fcfa00496a4d5a50317068ec8061297e2d2eb
utshamondal4418 <utshamondal4418@gmail.com>
2024-12-01 18:45:08 +0600

    alpha clash pro 1 done

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,50 @@
+// function play(){
+// //     // step-1: hide the home screen. to hide the screen add the class hidden to the home section
+//    const homeSection = document.getElementById('home-screen');
+//    homeSection.classList.add('hidden');
+// //    console.log(homeSection.classList)
+
+// //     // show the play ground
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+//     // console.log(playGroundSection.classList)
+// }
+
+// function handleKeyboardKeyUp(event){ 
+//     const playerPressed = event.key;
+//     console.log('player pressed', playerPressed);
+
+//     // get the expected to press
+//     const currentAlphabetElement = document.getElementById('current-alphabet');
+//     const currentAlphabet = currentAlphabetElement.innerText;
+//     const expectedAlphabet = currentAlphabet.toLowerCase();
+//     console.log(playerPressed, expectedAlphabet)
+
+//     // check matched or not
+//     if(playerPressed === expectedAlphabet){
+//         console.log('you get a point')
+//     }
+//     else{
+//         console.log('you missed. you lost a life');
+//     }
+// }
+// document.addEventListener('keyup', handleKeyboardKeyUp)
+
+function continueGame(){
+//     // step-1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet',alphabet);
+
+//     // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+//     // set background color
+    setBackgroundColorById(alphabet);
+}
+
+function play(){
+    hideElementById('home-screen')
+    showElementById('play-ground')
+    continueGame();
+}
